7da3f407f325aecb5bb467ac9433a1e36fed2e64
Stuart MacLean <stuart@blackomega.co.uk>
2021-12-22 19:40:04 +0000

    Modify cmake such that it looks for the Linux libraries in /usr/lib. Add install for remote deployment on Linux

--- source/build/cmake/musepack.cmake
+++ source/build/cmake/musepack.cmake
@@ -1,24 +1,33 @@
 # Musepack configuration
 
-set(MUSEPACK_VERSION "r475")
-if (OMEGA_WIN32)
-	set(MUSEPACK_HOME "${ATHENA_UTILS}/musepack/musepack-${MUSEPACK_VERSION}-${BUILD_SUFFIX}")
-else (OMEGA_WIN32)
-	set(MUSEPACK_HOME "${ATHENA_UTILS}/musepack/musepack-${MUSEPACK_VERSION}-${TIGER_PLATFORM}")
-endif (OMEGA_WIN32)
+if (${TIGER_LINUX_DISTRO})
+	set(MUSEPACK_HOME "${ATHENA_UTILS}/usr/lib")
 
-include_directories(AFTER "${MUSEPACK_HOME}/include" )
+	add_library(mpcdec SHARED IMPORTED)
+	set_property(TARGET mpcdec PROPERTY IMPORTED_LOCATION "${MUSEPACK_HOME}/libmpcdec.so" )
+	
+else (${TIGER_LINUX_DISTRO})
 
-if (OMEGA_WIN32)
-	set(MUSEPACK_LIBNAME "mpcdec")
-else (OMEGA_WIN32)
-	set(MUSEPACK_LIBNAME "libmpcdec")
-endif (OMEGA_WIN32)
+	set(MUSEPACK_VERSION "r475")
+	if (OMEGA_WIN32)
+		set(MUSEPACK_HOME "${ATHENA_UTILS}/musepack/musepack-${MUSEPACK_VERSION}-${BUILD_SUFFIX}")
+	else (OMEGA_WIN32)
+		set(MUSEPACK_HOME "${ATHENA_UTILS}/musepack/musepack-${MUSEPACK_VERSION}-${TIGER_PLATFORM}")
+	endif (OMEGA_WIN32)
 
-add_library(mpcdec SHARED IMPORTED)
-set_property(TARGET mpcdec PROPERTY IMPORTED_LOCATION "${MUSEPACK_HOME}/lib/${MUSEPACK_LIBNAME}.${LIBEXT}" )
-if (OMEGA_WIN32)
-	set_property(TARGET mpcdec PROPERTY IMPORTED_IMPLIB "${MUSEPACK_HOME}/lib/${MUSEPACK_LIBNAME}.lib" )
-endif (OMEGA_WIN32)
+	include_directories(AFTER "${MUSEPACK_HOME}/include" )
 
-message("${MUSEPACK_HOME}/lib/${MUSEPACK_LIBNAME}.${LIBEXT}")
+	if (OMEGA_WIN32)
+		set(MUSEPACK_LIBNAME "mpcdec")
+	else (OMEGA_WIN32)
+		set(MUSEPACK_LIBNAME "libmpcdec")
+	endif (OMEGA_WIN32)
+
+	add_library(mpcdec SHARED IMPORTED)
+	set_property(TARGET mpcdec PROPERTY IMPORTED_LOCATION "${MUSEPACK_HOME}/lib/${MUSEPACK_LIBNAME}.${LIBEXT}" )
+	if (OMEGA_WIN32)
+		set_property(TARGET mpcdec PROPERTY IMPORTED_IMPLIB "${MUSEPACK_HOME}/lib/${MUSEPACK_LIBNAME}.lib" )
+	endif (OMEGA_WIN32)
+
+	message("${MUSEPACK_HOME}/lib/${MUSEPACK_LIBNAME}.${LIBEXT}")
+endif (${TIGER_LINUX_DISTRO})
--- source/build/cmake/wavpack.cmake
+++ source/build/cmake/wavpack.cmake
@@ -1,24 +1,33 @@
 # WavPack configuration
 
-set(WAVPACK_VERSION "5.1.0")
-if (OMEGA_WIN32)
-	set(WAVPACK_HOME "${ATHENA_UTILS}/wavpack/wavpack-${WAVPACK_VERSION}-${BUILD_SUFFIX}")
-else (OMEGA_WIN32)
-	set(WAVPACK_HOME "${ATHENA_UTILS}/wavpack/wavpack-${WAVPACK_VERSION}-${TIGER_PLATFORM}")
-endif (OMEGA_WIN32)
+if (${TIGER_LINUX_DISTRO})
+	set(WAVPACK_HOME "${ATHENA_UTILS}/usr/lib")
 
-include_directories(AFTER "${WAVPACK_HOME}/include" )
+	add_library(wavpack SHARED IMPORTED)
+	set_property(TARGET wavpack PROPERTY IMPORTED_LOCATION "${WAVPACK_HOME}/libwavpack.so" )
+	
+else (${TIGER_LINUX_DISTRO})
 
-if (OMEGA_WIN32)
-	set(WAVPACK_LIBNAME "wavpackdll")
-else (OMEGA_WIN32)
-	set(WAVPACK_LIBNAME "libwavpack")
-endif (OMEGA_WIN32)
+	set(WAVPACK_VERSION "5.1.0")
+	if (OMEGA_WIN32)
+		set(WAVPACK_HOME "${ATHENA_UTILS}/wavpack/wavpack-${WAVPACK_VERSION}-${BUILD_SUFFIX}")
+	else (OMEGA_WIN32)
+		set(WAVPACK_HOME "${ATHENA_UTILS}/wavpack/wavpack-${WAVPACK_VERSION}-${TIGER_PLATFORM}")
+	endif (OMEGA_WIN32)
 
-add_library(wavpack SHARED IMPORTED)
-set_property(TARGET wavpack PROPERTY IMPORTED_LOCATION "${WAVPACK_HOME}/lib/${WAVPACK_LIBNAME}.${LIBEXT}" )
-if (OMEGA_WIN32)
-	set_property(TARGET wavpack PROPERTY IMPORTED_IMPLIB "${WAVPACK_HOME}/lib/${WAVPACK_LIBNAME}.lib" )
-endif (OMEGA_WIN32)
+	include_directories(AFTER "${WAVPACK_HOME}/include" )
+	
+	if (OMEGA_WIN32)
+		set(WAVPACK_LIBNAME "wavpackdll")
+	else (OMEGA_WIN32)
+		set(WAVPACK_LIBNAME "libwavpack")
+	endif (OMEGA_WIN32)
 
-message("${WAVPACK_HOME}/lib/${WAVPACK_LIBNAME}.${LIBEXT}")
+	add_library(wavpack SHARED IMPORTED)
+	set_property(TARGET wavpack PROPERTY IMPORTED_LOCATION "${WAVPACK_HOME}/lib/${WAVPACK_LIBNAME}.${LIBEXT}" )
+	if (OMEGA_WIN32)
+		set_property(TARGET wavpack PROPERTY IMPORTED_IMPLIB "${WAVPACK_HOME}/lib/${WAVPACK_LIBNAME}.lib" )
+	endif (OMEGA_WIN32)
+
+	message("${WAVPACK_HOME}/lib/${WAVPACK_LIBNAME}.${LIBEXT}")
+endif (${TIGER_LINUX_DISTRO})
